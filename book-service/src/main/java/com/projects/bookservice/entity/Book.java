23896b6305a02b83cfd2ac8e6db827dca1999f4c
jcilacad <johnchristopherilacad27@gmail.com>
2024-06-12 01:48:09 +0800

    Initilized book service. Added book entity.

---
+++ book-service/src/main/java/com/projects/bookservice/entity/Book.java
@@ -0,0 +1,32 @@
+package com.projects.bookservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "books_gen")
+    @SequenceGenerator(name = "books_gen", sequenceName = "books_seq", allocationSize = 1)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false, unique = true)
+    private String isbn;
+
+    private boolean isAvailable;
+}
